c329191c119f1245389581982d6867b5ffcc00e4
KatzeWien <daniela@sander.or.at>
2025-02-26 17:04:54 +0100

    db connection is working

---
+++ MonsterTrading/DBAccess.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTrading
+{
+    public class DBAccess
+    {
+        private string connString;
+        public DBAccess()
+        {
+            this.connString = "Host=localhost;Username=if23b258;Password=123456;Database=postgres";
+        }
+
+        public void Connect()
+        {
+            using var conn = new NpgsqlConnection(this.connString);
+            try
+            {
+                conn.Open();
+                Console.WriteLine("DB Connection successful");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- MonsterTrading/Server.cs
+++ MonsterTrading/Server.cs
@@ -10,14 +10,17 @@ namespace MonsterTrading
     public class Server
     {
         private TcpListener httpServer;
+        private DBAccess dbAccess;
         public Server(IPAddress address, int port)
         {
             this.httpServer = new TcpListener(address, port);
+            dbAccess = new DBAccess();
         }
 
         public void Start()
         {
             httpServer.Start();
+            dbAccess.Connect();
             while (true)
             {
                 var clientSocket = httpServer.AcceptTcpClient();
