3ff1284dc57afa8ba6b19a94e683dbad1ea33cac
KatzeWien <daniela@sander.or.at>
2025-02-26 16:16:56 +0100

    Connecting to Server possible

--- MonsterTrading/Program.cs
+++ MonsterTrading/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MonsterTrading;
+using System.Net;
+
+IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+int port = 10001;
+
+Server server = new Server(iPAddress, port);
+server.Start();
--- MonsterTrading/Server.cs
+++ MonsterTrading/Server.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace MonsterTrading
 {
     public class Server
     {
+        private TcpListener httpServer;
+        public Server(IPAddress address, int port)
+        {
+            this.httpServer = new TcpListener(address, port);
+        }
+
+        public void Start()
+        {
+            httpServer.Start();
+            while (true)
+            {
+                var clientSocket = httpServer.AcceptTcpClient();
+                Console.WriteLine("Hello");
+            }
+        }
     }
 }
