01e91f70670587739351f8ef8d8061130c14d8f0
KatzeWien <daniela@sander.or.at>
2025-02-28 19:12:50 +0100

    basic version of rest

--- MonsterTrading/Server.cs
+++ MonsterTrading/Server.cs
@@ -11,6 +11,10 @@ namespace MonsterTrading
     {
         private TcpListener httpServer;
         private DBAccess dbAccess;
+        private string? path;
+        private string? method;
+        private string? body;
+
         public Server(IPAddress address, int port)
         {
             this.httpServer = new TcpListener(address, port);
@@ -25,6 +29,102 @@ namespace MonsterTrading
             {
                 var clientSocket = httpServer.AcceptTcpClient();
                 Console.WriteLine("Hello");
+                GetEndpoint(clientSocket);
+            }
+        }
+
+        public void GetEndpoint(TcpClient clientSocket)
+        {
+            using var reader = new StreamReader(clientSocket.GetStream());
+            analyseRequest(reader);
+            if(this.path == "/users")
+            {
+                Console.WriteLine("work with user");
+            }
+            else if(this.path == "/sessions")
+            {
+                Console.WriteLine("work with sessions");
+            }
+            else if(this.path == "/packages")
+            {
+                Console.WriteLine("work with packages");
+            }
+            else if(this.path == "/transactions")
+            {
+                Console.WriteLine("work with transactions");
+            }
+            else if (this.path == "/cards")
+            {
+                Console.WriteLine("work with cards");
+            }
+            else if (this.path == "/deck")
+            {
+                Console.WriteLine("work with deck");
+            }
+            else if (this.path == "/scoreboard")
+            {
+                Console.WriteLine("work with scoreboard");
+            }
+            else if (this.path == "/battles")
+            {
+                Console.WriteLine("work with battles");
+            }
+            else if (this.path == "/stats")
+            {
+                Console.WriteLine("work with stats");
+            }
+            else if (this.path == "/tradings")
+            {
+                Console.WriteLine("work with tradings");
+            }
+            Console.WriteLine(method);
+            Console.WriteLine(body);
+        }
+
+        public void analyseRequest(StreamReader reader)
+        {
+            // ----- 1. Read the HTTP-Request -----
+            string? line;
+
+            // 1.1 first line in HTTP contains the method, path and HTTP version
+            line = reader.ReadLine();
+            if (line != null)
+            {
+                var partsOfFirstLine = line.Split(" ");
+                this.method = partsOfFirstLine[0];
+                this.path = partsOfFirstLine[1];
+            }
+            // 1.2 read the HTTP-headers (in HTTP after the first line, until the empy line)
+            int content_length = 0; // we need the content_length later, to be able to read the HTTP-content
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line == "")
+                {
+                    break;  // empty line indicates the end of the HTTP-headers
+                }
+
+                // Parse the header
+                var parts = line.Split(':');
+                if (parts.Length == 2 && parts[0].Trim() == "Content-Length")
+                {
+                    content_length = int.Parse(parts[1].Trim());
+                }
+            }
+            // 1.3 read the body if existing
+            if (content_length > 0)
+            {
+                var data = new StringBuilder(200);
+                char[] chars = new char[1024];
+                int bytesReadTotal = 0;
+                while (bytesReadTotal < content_length)
+                {
+                    var bytesRead = reader.Read(chars, 0, chars.Length);
+                    bytesReadTotal += bytesRead;
+                    if (bytesRead == 0)
+                        break;
+                    data.Append(chars, 0, bytesRead);
+                }
+                this.body = (data.ToString());
             }
         }
     }
